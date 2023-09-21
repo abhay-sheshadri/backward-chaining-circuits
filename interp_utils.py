0c50a3980686bd9c44e0b98288ecc55d008f13c1
victorLevoso <vlfernandez@esei.uvigo.es>
2023-09-21 15:57:17 +0200

    aded some notebooks with extra experiments

--- interp_utils.py
+++ interp_utils.py
@@ -188,6 +188,88 @@ def activation_patching(model, dataset, clean_tokens, corrupted_tokens, comparis
             patching_result[layer, position] = (patched_logit_diff - corrupted_logit_diff) / normalize_ratio
     return patching_result
 
+def activation_patching_mlp_pre(model, dataset, clean_tokens, corrupted_tokens, comparison_index):
+    # We run on the clean prompt with the cache so we store activations to patch in later.
+    clean_logits, clean_cache = model.run_with_cache(clean_tokens)
+    clean_logit_diff = logits_to_logit_diff(clean_tokens, corrupted_tokens, clean_logits, comparison_index)
+    print(f"Clean logit difference: {clean_logit_diff.item():.3f}")
+
+    # We don't need to cache on the corrupted prompt.
+    corrupted_logits = model(corrupted_tokens)
+    corrupted_logit_diff = logits_to_logit_diff(clean_tokens, corrupted_tokens, corrupted_logits, comparison_index)
+    print(f"Corrupted logit difference: {corrupted_logit_diff.item():.3f}")
+    print(f"Positive Direction: {dataset.idx2tokens[clean_tokens[comparison_index]]}")
+    print(f"Negative Direction: {dataset.idx2tokens[corrupted_tokens[comparison_index]]}")
+
+    def mlp_patching_hook(
+        resid_pre,
+        hook,
+        position):
+        # Each HookPoint has a name attribute giving the name of the hook.
+        clean_resid_pre = clean_cache[hook.name]
+        resid_pre[:, position, :] = clean_resid_pre[:, position, :]
+        return resid_pre
+    # We make a tensor to store the results for each patching run. We put it on the model's device to avoid needing to move things between the GPU and CPU, which can be slow.
+    num_positions = clean_tokens.shape[0]
+    patching_result = torch.zeros((model.cfg.n_layers, num_positions), device=model.cfg.device)
+    for layer in tqdm_auto.tqdm(range(model.cfg.n_layers)):
+        for position in range(num_positions):
+            # Use functools.partial to create a temporary hook function with the position fixed
+            temp_hook_fn = partial(mlp_patching_hook, position=position)
+            # Run the model with the patching hook
+            patched_logits = model.run_with_hooks(corrupted_tokens, fwd_hooks=[
+                (tl_util.get_act_name("pre", layer), temp_hook_fn)
+            ])
+            # Calculate the logit difference
+            patched_logit_diff = logits_to_logit_diff(clean_tokens, corrupted_tokens, patched_logits, comparison_index).detach()
+            # Store the result, normalizing by the clean and corrupted logit difference so it's between 0 and 1 (ish)
+            normalize_ratio = (clean_logit_diff - corrupted_logit_diff)
+            if normalize_ratio == 0:
+                normalize_ratio = 1
+            patching_result[layer, position] = (patched_logit_diff - corrupted_logit_diff) / normalize_ratio
+    return patching_result
+
+def activation_patching_mlp_out(model, dataset, clean_tokens, corrupted_tokens, comparison_index):
+    # We run on the clean prompt with the cache so we store activations to patch in later.
+    clean_logits, clean_cache = model.run_with_cache(clean_tokens)
+    clean_logit_diff = logits_to_logit_diff(clean_tokens, corrupted_tokens, clean_logits, comparison_index)
+    print(f"Clean logit difference: {clean_logit_diff.item():.3f}")
+
+    # We don't need to cache on the corrupted prompt.
+    corrupted_logits = model(corrupted_tokens)
+    corrupted_logit_diff = logits_to_logit_diff(clean_tokens, corrupted_tokens, corrupted_logits, comparison_index)
+    print(f"Corrupted logit difference: {corrupted_logit_diff.item():.3f}")
+    print(f"Positive Direction: {dataset.idx2tokens[clean_tokens[comparison_index]]}")
+    print(f"Negative Direction: {dataset.idx2tokens[corrupted_tokens[comparison_index]]}")
+
+    def mlp_patching_hook(
+        resid_pre,
+        hook,
+        position):
+        # Each HookPoint has a name attribute giving the name of the hook.
+        clean_resid_pre = clean_cache[hook.name]
+        resid_pre[:, position, :] = clean_resid_pre[:, position, :]
+        return resid_pre
+    # We make a tensor to store the results for each patching run. We put it on the model's device to avoid needing to move things between the GPU and CPU, which can be slow.
+    num_positions = clean_tokens.shape[0]
+    patching_result = torch.zeros((model.cfg.n_layers, num_positions), device=model.cfg.device)
+    for layer in tqdm_auto.tqdm(range(model.cfg.n_layers)):
+        for position in range(num_positions):
+            # Use functools.partial to create a temporary hook function with the position fixed
+            temp_hook_fn = partial(mlp_patching_hook, position=position)
+            # Run the model with the patching hook
+            patched_logits = model.run_with_hooks(corrupted_tokens, fwd_hooks=[
+                (tl_util.get_act_name("mlp_out", layer), temp_hook_fn)
+            ])
+            # Calculate the logit difference
+            patched_logit_diff = logits_to_logit_diff(clean_tokens, corrupted_tokens, patched_logits, comparison_index).detach()
+            # Store the result, normalizing by the clean and corrupted logit difference so it's between 0 and 1 (ish)
+            normalize_ratio = (clean_logit_diff - corrupted_logit_diff)
+            if normalize_ratio == 0:
+                normalize_ratio = 1
+            patching_result[layer, position] = (patched_logit_diff - corrupted_logit_diff) / normalize_ratio
+    return patching_result
+
 
 def plot_activations(patching_result, clean_tokens, dataset):
     # Add the index to the end of the label, because plotly doesn't like duplicate labels
